ff6cd76277be06741b38d08d5b3eb5f8ec008156
minhlc98 <minhlc98@gmail.com>
2022-02-03 13:43:53 +0700

    [ADD] docker compose

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const crons = require("./crons");
 
 const PORT = process.env.PORT || 8080;
-const DB_CONNECTION = process.env.DB_CONNECTION || "mongodb://localhost:27017/myapp";
+const DB_CONNECTION = process.env.DB_CONNECTION || "mongodb://db:27017/myapp";
 
 mongoose.connect(
   DB_CONNECTION,
@@ -19,6 +19,7 @@ mongoose.connect(
     if (err) {
       throw err;
     }
+    console.log("Connect success to mongodb");
     crons.start();
   }
 );
