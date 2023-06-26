94debe8d8c159dac963aeb97ba2fa250f65aaa1f
Cam Minh <minhlc98@gmail.com>
2023-06-26 23:21:29 +0700

    [ADD] eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"commonjs": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+	}
+}
