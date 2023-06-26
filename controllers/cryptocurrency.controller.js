b784692ffb02d7fea4081a80c45abadd829b576b
Cam Minh <minhlc98@gmail.com>
2023-06-26 23:49:49 +0700

    [ADD] api get cryptocurrency by slug

--- controllers/cryptocurrency.controller.js
+++ controllers/cryptocurrency.controller.js
@@ -3,6 +3,11 @@ const CryptocurrencyMD = require("../models/cryptocurrency");
 module.exports = {
   get: async (req, res) => {
     const data = await CryptocurrencyMD.find().sort({ cmc_rank: 1 }).lean(true);
-    return res.json({ data });
+    return res.json(data);
+  },
+  getBySlug: async (req, res) => {
+    const slug = req.body.slug;
+    const data = await CryptocurrencyMD.findOne({ slug }).lean(true);
+    return res.json(data);
   }
 }
--- routers/cryptocurrency.routes.js
+++ routers/cryptocurrency.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CryptocurrencyController = require("../controllers/cryptocurrency");
 
 router.get("/", CryptocurrencyController.get);
+router.get("/:slug", CryptocurrencyController.getBySlug);
 
 module.exports = router;
